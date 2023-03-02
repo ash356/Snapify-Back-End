221ec88fa06c412a24238afdd0aed363f9e9e8c6
Ahmed Ashour <you@example.com>
2023-03-02 23:46:16 +0200

    update-02

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
   return await mongoose
-    .connect(process.env.DB_LOCAL)
+    .connect(process.env.DB_ATLAS)
     .then((result) => {
       console.log(`DB Connected...`);
       //   console.log(result);
